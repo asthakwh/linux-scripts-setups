d7ef9c4d319ed1e90020c86d71bd606189e9547a
Astha Kushwaha <130935853+asthakwh@users.noreply.github.com>
2026-05-09 15:10:37 +0530

    Create package-commands.sh

---
+++ package-commands.sh
@@ -0,0 +1,10 @@
+ sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl git python3
+curl --version
+git --version
+python3 --version
+
+# To remove package
+sudo apt remove -y htop
+history
